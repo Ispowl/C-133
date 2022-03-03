1ddc2e0f95c200bd60def071c0a3b147818d1a6a
Ispowl <86279288+Ispowl@users.noreply.github.com>
2022-03-03 18:28:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+img = "";
+status = "";
+object = [];
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status = detecting objects";
+}
+function draw(){
+    image(img,0,0,640,420);
+    
+    if(status !=""){
+      for(i = 0; i <object.length; i++)  {
+          document.getElementById("status").innerHTML = "Status = DETECTED OBJECTS [sona!]";
+          fill("#FF0000");
+          percent = floor(object[i].confidence * 100);
+          text(object[i].label+""+percent+"%",object[i].x,object[i].y);
+          noFill();
+          stroke("#FF0000");
+          rect(object[i].x,object[i].y,object[i].width,object[i].height);
+      }
+    }
+}
+
+function modelLoaded(){
+  console.log('Model loaded!');
+  status = true;
+  objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    object = results;
+
+    
+}
